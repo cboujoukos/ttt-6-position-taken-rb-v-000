99cff35aea5b9b9ee26c3d589eabdb0d1326a894
Christopher Michael Boujoukos <cboujoukos@gmail.com>
2017-11-06 16:43:59 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,6 @@
 def position_taken?(board, index)
-  if board[index] == " " || ""
+  if board[index] == " " || "" || nil
     return false
-  elsif board[index] == "X" || "O"
-    return true
-  end
   else
     return true
 end
